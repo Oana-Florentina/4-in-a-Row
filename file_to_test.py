94afa4b759b50a164d2ff892144f079a5c101357
Florentina <floryflorentina55@gmail.com>
2023-12-29 20:08:45 +0200

    player vs player

--- file_to_test.py
+++ file_to_test.py
@@ -128,3 +128,120 @@ def draw_hover_piece(screen, col, turn):
         pygame.draw.circle(screen, YELLOW, (mouse_x, int(Piece_size / 2)), RADIUS)
 
 
+# Main game functions player vs player
+
+def Game_over(board, piece):
+    # Check horizontal locations for win
+    for col in range(COLS - 3):
+        for row in range(ROWS):
+            if board[row][col] == piece and board[row][col + 1] == piece and board[row][col + 2] == piece and \
+                    board[row][col + 3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for col in range(COLS):
+        for row in range(ROWS - 3):
+            if board[row][col] == piece and board[row + 1][col] == piece and board[row + 2][col] == piece and \
+                    board[row + 3][col] == piece:
+                return True
+
+    # Check positively sloped diaganols
+    for col in range(COLS - 3):
+        for row in range(ROWS - 3):
+            if board[row][col] == piece and board[row + 1][col + 1] == piece and board[row + 2][col + 2] == piece and \
+                    board[row + 3][col + 3] == piece:
+                return True
+
+    # Check negatively sloped diaganols
+    for col in range(COLS - 3):
+        for row in range(3, ROWS):
+            if board[row][col] == piece and board[row - 1][col + 1] == piece and board[row - 2][col + 2] == piece and \
+                    board[row - 3][col + 3] == piece:
+                return True
+
+    return False
+
+
+def display_message(screen, message):
+    font = pygame.font.Font(None, 36)
+    text = font.render(message, True, WHITE)
+    text_rect = text.get_rect(center=(COLS * Piece_size // 2, Piece_size // 2))
+    screen.blit(text, text_rect)
+    pygame.display.update()
+
+
+def game_two_players(screen, board):
+    turn = PLAYER_ONE
+    game_over = Game_over(board, PLAYER_ONE_PIECE)
+    draw_board(screen, board)
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+            if is_tie(board):
+                game_over = True
+                break
+            if event.type == pygame.MOUSEMOTION:
+                col = int(event.pos[0] / Piece_size)
+                draw_hover_piece(screen, col, turn)
+                pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                if turn == PLAYER_ONE:
+                    col = int(event.pos[0] / Piece_size)
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        add_piece(board, row, col, PLAYER_ONE_PIECE)
+                        if Game_over(board, PLAYER_ONE_PIECE):
+                            print("PLAYER 1 WINS!")
+                            game_over = True
+                        turn = PLAYER_TWO
+
+                else:
+                    col = int(event.pos[0] / Piece_size)
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        add_piece(board, row, col, PLAYER_TWO_PIECE)
+                        if Game_over(board, PLAYER_TWO_PIECE):
+                            print("PLAYER 2 WINS!")
+                            game_over = True
+                        turn = PLAYER_ONE
+
+                print_board(board)
+                draw_board(screen, board)
+                pygame.display.update()
+
+                col = int(event.pos[0] / Piece_size)
+                draw_hover_piece(screen, col, turn)
+                pygame.display.update()
+    if game_over:
+        if Game_over(board, PLAYER_ONE_PIECE):
+            display_message(screen, "PLAYER 1 WINS!")
+        elif Game_over(board, PLAYER_TWO_PIECE):
+            display_message(screen, "PLAYER 2 WINS!")
+        elif is_tie(board):
+            display_message(screen, "IT'S TIE")
+
+    # after 5 seconds close the game
+    pygame.time.wait(5000)
+    pygame.quit()
+
+def main():
+
+
+    initialize_rows_cols()
+    board = create_board()
+    pygame.init()
+    screen = pygame.display.set_mode((COLS * Piece_size, (ROWS + 1) * Piece_size))
+    pygame.display.set_caption('Connect 4')
+    game_two_players(screen, board)
+
+
+
+
+if __name__ == '__main__':
+    main()
