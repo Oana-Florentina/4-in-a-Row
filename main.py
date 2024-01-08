5488526369e65e5409490267a49c93e6096b23da
Florentina <floryflorentina55@gmail.com>
2024-01-08 22:53:27 +0200

    added documentation and final fixes.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-DARK_GRAY = (169, 169, 169)
+DARK_GRAY = (30, 30, 30)
 # ----------------
 
 # Players
@@ -44,8 +44,7 @@ PLAYING = True
 # ----------------
 
 
-# Board configuration functions
-
+# Command line arguments
 try:
     opponent = sys.argv[1]
     ROWS = int(sys.argv[2])
@@ -57,20 +56,44 @@ except ValueError:
     sys.exit(1)
 
 
+# Board configuration functions
+# -----------------------------
 def create_board():
+    """
+    Creates a board with the specified number of rows and columns.
+    :return: A numpy array representing the board.
+    """
     board = np.zeros((ROWS, COLS))
     return board
 
 
 def print_board(board):
+    """
+    Prints the board to the console. The board is flipped vertically so that the first row is at the bottom.
+    :param board:
+    """
     print(np.flip(board, 0))
 
 
 def is_valid_location(board, col):
+    """
+    Checks if the specified column is a valid location for a piece to be placed.
+    :param board:
+    :param col:
+    :return: True if the column is valid, False otherwise.
+    """
     return board[ROWS - 1][col] == 0
 
 
 def get_next_open_row(board, col):
+    """
+    Gets the next open row in the specified column.
+
+    This method will be used to determine where to place the piece, on a valid column.
+    :param board:
+    :param col:
+    :return: the next open row in the specified column, or -1 if the column is full.
+    """
     for row in range(ROWS):
         if board[row][col] == 0:
             return row
@@ -78,10 +101,22 @@ def get_next_open_row(board, col):
 
 
 def add_piece(board, row, col, piece):
+    """
+    Adds a piece to the specified location on the board.
+    :param board:
+    :param row: the row to add the piece to.
+    :param col: the column to add the piece to.
+    :param piece: the piece to add to the board.
+    """
     board[row][col] = piece
 
 
 def is_tie(board):
+    """
+    Checks if the board is full, which means the game is a tie.
+    :param board:
+    :return: true if the board is full, false otherwise.
+    """
     for col in range(COLS):
         if board[ROWS - 1][col] == 0:
             return False
@@ -91,6 +126,10 @@ def is_tie(board):
 # Game GUI functions
 
 def board_setup():
+    """
+    Sets up the board and the screen.
+    :return: the screen, with the board drawn on it.
+    """
     pygame.init()
     screen_width = COLS * Piece_size
     screen_height = (ROWS + 2) * Piece_size  # Increased by one row for the text and one for the board shift
@@ -100,6 +139,13 @@ def board_setup():
 
 
 def draw_border(screen, color):
+    """
+    Draws a border around the board, with the specified color.
+
+    This method will be used to indicate the winner, by using the color of the winning player.
+    :param screen:
+    :param color: the color of the border.
+    """
 
     border_thickness = 5
     border_color = color
@@ -120,6 +166,11 @@ def draw_border(screen, color):
 
 
 def draw_board(screen, board):
+    """
+    Draws the board on the screen.
+    :param screen:
+    :param board:
+    """
 
     for col in range(COLS):
         for row in range(ROWS):
@@ -146,6 +197,14 @@ def draw_board(screen, board):
 
 
 def draw_hover_piece(screen, col, turn):
+    """
+    Draws a piece on the top of the board, to indicate where the player will place their piece.
+    The piece is drawn with the color of the player whose turn it is.
+    The position of the piece is determined by the mouse position.
+    :param screen:
+    :param col:
+    :param turn: the player whose turn it is.
+    """
     mouse_x = col * Piece_size + int(Piece_size / 2)
 
     pygame.draw.rect(screen, DARK_GRAY, (0, 0, COLS * Piece_size, Piece_size))
@@ -159,6 +218,12 @@ def draw_hover_piece(screen, col, turn):
 # Main game functions player vs player
 
 def is_game_over(board, piece):
+    """
+    Checks if the specified piece has won the game.
+    :param board:
+    :param piece: the piece to check for a win.
+    :return: true if the piece has won, false otherwise.
+    """
     # Check horizontal locations for win
     for col in range(COLS - 3):
         for row in range(ROWS):
@@ -191,6 +256,13 @@ def is_game_over(board, piece):
 
 
 def display_message(screen, message):
+    """
+    Displays a message on the screen.
+    Used to indicate the winner or a tie.
+    :param screen:
+    :param message:
+    :return:
+    """
     font = pygame.font.Font(None, 36)
     text = font.render(message, True, BLACK)
     text_rect = text.get_rect(center=(COLS * Piece_size // 2, Piece_size // 2))
@@ -199,10 +271,17 @@ def display_message(screen, message):
 
 
 def game_two_players(screen, board):
+    """
+    Main game loop for player vs player mode.
+    :param screen:
+    :param board:
+    :return:
+    """
     turn = PLAYER_ONE
     game_over = is_game_over(board, PLAYER_ONE_PIECE)
     draw_board(screen, board)
 
+    # loop until the game is over
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -260,7 +339,6 @@ def game_two_players(screen, board):
             display_message(screen, "IT'S TIE")
             draw_border(screen, WHITE)
 
-
     # after 5 seconds close the game
     pygame.time.wait(5000)
     pygame.quit()
@@ -269,6 +347,13 @@ def game_two_players(screen, board):
 # Main game functions player vs AI
 
 def evaluate_block(window, piece):
+    """
+    Evaluates a block of 4 pieces.
+    Used to determine the score of a window.
+    :param window: A block of 4 pieces that is being evaluated. Can be a row, column, or diagonal.
+    :param piece: The piece of the player whose score is being calculated.
+    :return: the score of the block.
+    """
     piece_count = np.count_nonzero(window == piece)
     empty_count = np.count_nonzero(window == EMPTY)
     score = 0
@@ -281,7 +366,6 @@ def evaluate_block(window, piece):
     elif piece_count == 2 and empty_count == 2:
         score += 10
 
-
     opposite_count = np.count_nonzero(window == opp_piece)
     if opposite_count == 3 and empty_count == 1:
         score -= 50
@@ -290,6 +374,12 @@ def evaluate_block(window, piece):
 
 
 def extract_blocks(board):
+    """
+    Extracts all the windows from the board.
+    Used to calculate the score of the board.
+    :param board:
+    :return: a list of all the windows in the board.
+    """
     windows = []
     rows, cols = ROWS, COLS
 
@@ -305,13 +395,13 @@ def extract_blocks(board):
             window = [board[r + i][c] for i in range(4)]
             windows.append(window)
 
-    # Positive sloped diagonal windows
+    # / diagonal windows
     for r in range(rows - 3):
         for c in range(cols - 3):
             window = [board[r + i][c + i] for i in range(4)]
             windows.append(window)
 
-    # Negative sloped diagonal windows
+    # \ diagonal windows
     for r in range(rows - 3):
         for c in range(cols - 3):
             window = [board[r + 3 - i][c + i] for i in range(4)]
@@ -321,9 +411,15 @@ def extract_blocks(board):
 
 
 def calculate_score(board, piece):
+    """
+    Calculates the score of the board for the specified player.
+    :param board:
+    :param piece:
+    :return:
+    """
     score = 0
     blocks = extract_blocks(board)
-
+    # Calculate score for each window and add it to the total score
     for window in blocks:
         score += evaluate_block(window, piece)
 
@@ -342,11 +438,21 @@ def calculate_score(board, piece):
 
 
 def is_terminal_node(board):
+    """
+    Checks if the board is in a terminal state (game over or tie).
+    :param board:
+    :return: true if the board is in a terminal state, false otherwise.
+    """
     return is_game_over(board, PLAYER_ONE_PIECE) or is_game_over(board, AI_PLAYER_PIECE) or len(
         get_valid_locations(board)) == 0
 
 
 def get_valid_locations(board):
+    """
+    Gets all the valid locations for a piece to be placed.
+    :param board:
+    :return: a list of all the valid locations.
+    """
     valid_locations = []
     for col in range(COLS):
         if is_valid_location(board, col):
@@ -355,6 +461,13 @@ def get_valid_locations(board):
 
 
 def evaluate_terminal(board):
+    """
+    Evaluates the board in a terminal state.
+    Used to determine the score of the board.
+    :param board:
+    :return: a large positive number if the AI player has won, a large negative number
+    if the human player has won, 0 if it's a tie.
+    """
     if is_game_over(board, AI_PLAYER_PIECE):
         return 100000000000000
     elif is_game_over(board, PLAYER_ONE_PIECE):
@@ -364,10 +477,29 @@ def evaluate_terminal(board):
 
 
 def evaluate_depth_zero(board):
+    """
+    Evaluates the board in a non-terminal state.
+    Used to determine the score of the board.
+    :param board:
+    :return: the score of the board.
+    """
     return calculate_score(board, AI_PLAYER_PIECE)
 
 
 def minimax(board, depth, alpha, beta, maximizingPlayer):
+    """
+    Minimax algorithm with alpha-beta pruning.
+    Used to determine the best move for the AI player.
+    Source: https://www.hackerearth.com/blog/developers/minimax-algorithm-alpha-beta-pruning/
+
+    Source: https://www.youtube.com/watch?v=l-hh51ncgDI
+    :param board: the board to evaluate.
+    :param depth: the depth of the search tree.
+    :param alpha: the best value that the maximizing player currently can guarantee at that level or above.
+    :param beta: the best value that the minimizing player currently can guarantee at that level or above.
+    :param maximizingPlayer: true if the current player is the maximizing player, false otherwise.
+    :return: the best column to place a piece in, and the score of the board.
+    """
     valid_locations = get_valid_locations(board)
     is_terminal = is_terminal_node(board)
 
@@ -391,6 +523,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 
             alpha = max(alpha, value)
             if alpha >= beta:
+                # pruning because we already found a better move
                 break
 
         return column, value
@@ -417,8 +550,16 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 
 
 def game_vs_AI(screen, board, difficulty, first_player):
+    """
+    Main game loop for player vs AI mode.
+    :param screen:
+    :param board:
+    :param difficulty: The difficulty of the AI player. Can be 'easy', 'medium', or 'hard'.
+    :param first_player: The player who goes first. Can be 'human' or 'computer'.
+    """
     depth = 5
     turn = AI_PLAYER
+
     random_move = False
     if difficulty == "easy":
         random_move = True
@@ -442,6 +583,8 @@ def game_vs_AI(screen, board, difficulty, first_player):
 
             if event.type == pygame.MOUSEMOTION and turn == PLAYER_ONE:
                 col = int(event.pos[0] / Piece_size)
+                # event.pos[0] is the x coordinate of the mouse,
+                # / Piece_size is to get the column
                 draw_hover_piece(screen, col, turn)
                 pygame.display.update()
 
@@ -468,7 +611,7 @@ def game_vs_AI(screen, board, difficulty, first_player):
 
             if turn == AI_PLAYER:
                 if not random_move:
-                    col = minimax(board, depth, -math.inf, math.inf, True)[0]
+                    col = minimax(board, depth, MINUSINF, PLUSINF, True)[0]
                 else:
                     col = random.randint(0, COLS - 1)  # Random move cause how else can I make the game easy?
                 if is_valid_location(board, col):
@@ -502,13 +645,19 @@ def game_vs_AI(screen, board, difficulty, first_player):
             display_message(screen, "IT'S TIE")
             draw_border(screen, BLUE)
 
-
     # after 5 seconds close the game
     pygame.time.wait(5000)
     pygame.quit()
 
 
+# Argument validation functions
+
 def check_difficulty_is_valid(difficulty):
+    """
+    Checks if the specified difficulty is valid.
+    :param difficulty: Can be 'easy', 'medium', or 'hard'.
+    :return: True if the difficulty is valid, False otherwise.
+    """
     if difficulty == "easy" or difficulty == "medium" or difficulty == "hard":
         return True
     else:
@@ -516,22 +665,33 @@ def check_difficulty_is_valid(difficulty):
 
 
 def check_first_player_is_valid(first_player):
+    """
+    Checks if the specified first player is valid.
+    :param first_player: Can be 'human' or 'computer'.
+    :return: True if the first player is valid, False otherwise.
+    """
     if first_player == "human" or first_player == "computer":
         return True
     else:
         return False
+# -----------------------------
 
 
 def main():
+    """
+    Main function.
+    """
     board = create_board()
     pygame.init()
+
+    # Argument validation
+    # -------------------
 
     if ROWS < 4 or COLS < 4 or ROWS > 10 or COLS > 10:
         print("Rows and columns must be between 4 and 10.")
         sys.exit(1)
 
     screen = pygame.display.set_mode((COLS * Piece_size, (ROWS + 1) * Piece_size))
-
 
     pygame.display.set_caption('Connect 4 - Made by Flory')
 
@@ -570,6 +730,6 @@ def main():
         print("Invalid opponent. Choose from 'human' or 'computer'.")
         sys.exit(1)
 
-
+# Run the game
 if __name__ == '__main__':
     main()
